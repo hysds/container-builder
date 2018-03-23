7fb759a5fe2fd825339e87c8501bede6cc641a35
Gerald Manipon <pymonger@gmail.com>
2018-03-23 16:32:52 +0000

    propagate existing allowed_accounts
    Closes #870.

--- io-met.py
+++ io-met.py
@@ -1,33 +1,51 @@
 #!/usr/bin/env python
 from __future__ import print_function
-import sys
-import json
-import os
-import re
+import os, sys, json, re
 
-import hysds_commons.request_utils
+from hysds_commons.request_utils import requests_json_response
 from hysds.celery import app
 import utils
 
+
+def add_hysds_io(rest_url, metadata):
+    """Index hysds-io into respective components ES preserving allowed_accounts
+    if hysds-io already is registered."""
+
+    # get hysds-io if exists
+    doc = requests_json_response("GET", os.path.join(rest_url, "hysds_io/type"),
+                                 data={ "id": metadata['id'] }, verify=False,
+                                 ignore_errors=True)
+
+    # copy existing allowed accounts
+    if doc is not None:
+        metadata['allowed_accounts'] = list(metadata['allowed_accounts'] + 
+                                            doc['result'].get('allowed_accounts', []))
+
+    # index
+    requests_json_response("POST", os.path.join(rest_url ,"hysds_io/add"),
+                           data={ "spec":json.dumps(metadata) }, verify=False) 
+
+
 def usage_and_exit():
-    '''
-    Prints usage and exit
-    '''
-    print("Usage:\n\t{0} <hysds-io> <job-spec> <version>".format(sys.argv[0]),file=sys.stderr)
+    """Prints usage and exit"""
+
+    print("Usage:\n\t{0} <hysds-io> <job-spec> <version>".format(sys.argv[0]), file=sys.stderr)
     sys.exit(-1)
 
+
 if __name__ == "__main__":
-    '''
-    Main program routing arguments to file
-    '''
-    if len(sys.argv) != 4:
-        usage_and_exit()
+    """Main program routing arguments to file"""
+
+    if len(sys.argv) != 4: usage_and_exit()
+
     #Read arguments
     specification = sys.argv[1]
     job_spec = sys.argv[2]
-    version=sys.argv[3]
+    version = sys.argv[3]
+
     #Generate product name
-    product=utils.get_product_id(specification,version)
+    product = utils.get_product_id(specification, version)
+
     #Prepare dataset metadata
     metadata = {
         "job-specification": job_spec,
@@ -37,12 +55,13 @@ if __name__ == "__main__":
     if not utils.check_exists(job_spec):
         print("[ERROR] Job Specification, {0}, does not exist. Cannot create HySDS-IO.".format(job_spec),file=sys.stderr)
         sys.exit(-2)
+
     #Read specification metadata and merge it
     with open(specification,"r") as fp:
         metadata.update(json.load(fp))
     metadata["id"] = product
     if metadata.get("component","tosca") == "mozart":
-        hysds_commons.request_utils.requests_json_response("POST", os.path.join(app.conf["MOZART_REST_URL"],"hysds_io/add"), data={"spec":json.dumps(metadata)}, verify=False) 
+        add_hysds_io(app.conf["MOZART_REST_URL"], metadata)
     else:
-        hysds_commons.request_utils.requests_json_response("POST", os.path.join(app.conf["GRQ_REST_URL"],"hysds_io/add"), data={"spec":json.dumps(metadata)}, verify=False)
+        add_hysds_io(app.conf["GRQ_REST_URL"], metadata)
     sys.exit(0)
