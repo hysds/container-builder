0d28e683b6316196c36790a7b71dae2e7231b20f
Gerald Manipon <pymonger@gmail.com>
2018-09-20 16:57:13 -0700

    remove dependency on celeryconfig.py for mozart and grq rest urls

--- io-met.py
+++ io-met.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 from __future__ import print_function
-import os, sys, json, re
+import os
+import sys
+import json
+import re
 
 from hysds_commons.request_utils import requests_json_response
-from hysds.celery import app
 import utils
 
 
@@ -13,57 +15,60 @@ def add_hysds_io(rest_url, metadata):
 
     # get hysds-io if exists
     doc = requests_json_response("GET", os.path.join(rest_url, "hysds_io/type"),
-                                 data={ "id": metadata['id'] }, verify=False,
+                                 data={"id": metadata['id']}, verify=False,
                                  ignore_errors=True)
 
     # copy existing allowed accounts
     if doc is not None:
-        merged_accounts = list(set(metadata.get('allowed_accounts', []) + 
+        merged_accounts = list(set(metadata.get('allowed_accounts', []) +
                                    doc['result'].get('allowed_accounts', [])))
         if len(merged_accounts) > 0:
             metadata['allowed_accounts'] = merged_accounts
 
     # index
-    requests_json_response("POST", os.path.join(rest_url ,"hysds_io/add"),
-                           data={ "spec":json.dumps(metadata) }, verify=False) 
+    requests_json_response("POST", os.path.join(rest_url, "hysds_io/add"),
+                           data={"spec": json.dumps(metadata)}, verify=False)
 
 
 def usage_and_exit():
     """Prints usage and exit"""
 
-    print("Usage:\n\t{0} <hysds-io> <job-spec> <version>".format(sys.argv[0]), file=sys.stderr)
+    print("Usage:\n\t{0} <hysds-io> <job-spec> <version> <mozart_rest_url> <grq_rest_url>".format(sys.argv[0]), file=sys.stderr)
     sys.exit(-1)
 
 
 if __name__ == "__main__":
     """Main program routing arguments to file"""
 
-    if len(sys.argv) != 4: usage_and_exit()
+    if len(sys.argv) != 6:
+        usage_and_exit()
 
-    #Read arguments
+    # Read arguments
     specification = sys.argv[1]
     job_spec = sys.argv[2]
     version = sys.argv[3]
+    mozart_rest_url = sys.argv[4]
+    grq_rest_url = sys.argv[5]
 
-    #Generate product name
+    # Generate product name
     product = utils.get_product_id(specification, version)
 
-    #Prepare dataset metadata
+    # Prepare dataset metadata
     metadata = {
         "job-specification": job_spec,
         "job-version": version,
-        "resource":"hysds-io-specification"
+        "resource": "hysds-io-specification"
     }
-    if not utils.check_exists(job_spec):
-        print("[ERROR] Job Specification, {0}, does not exist. Cannot create HySDS-IO.".format(job_spec),file=sys.stderr)
+    if not utils.check_exists(job_spec, mozart_rest_url):
+        print("[ERROR] Job Specification, {0}, does not exist. Cannot create HySDS-IO.".format(job_spec), file=sys.stderr)
         sys.exit(-2)
 
-    #Read specification metadata and merge it
-    with open(specification,"r") as fp:
+    # Read specification metadata and merge it
+    with open(specification, "r") as fp:
         metadata.update(json.load(fp))
     metadata["id"] = product
-    if metadata.get("component","tosca") == "mozart":
-        add_hysds_io(app.conf["MOZART_REST_URL"], metadata)
+    if metadata.get("component", "tosca") == "mozart":
+        add_hysds_io(mozart_rest_url, metadata)
     else:
-        add_hysds_io(app.conf["GRQ_REST_URL"], metadata)
+        add_hysds_io(grq_rest_url, metadata)
     sys.exit(0)
--- job-met.py
+++ job-met.py
@@ -5,39 +5,39 @@ import json
 import os
 import re
 import hysds_commons.request_utils
-from hysds.celery import app
 import utils
+
 
 def usage_and_exit():
     '''
     Prints usage and exit
     '''
-    print("Usage:\n\t{0} <job-spec> <container> <version>".format(sys.argv[0]),file=sys.stderr)
+    print("Usage:\n\t{0} <job-spec> <container> <version>".format(sys.argv[0]), file=sys.stderr)
     sys.exit(-1)
 
+
 if __name__ == "__main__":
-    '''
-    Main program routing arguments to file
-    '''
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         usage_and_exit()
-    #Read arguments
-    specification=sys.argv[1]
-    container=sys.argv[2]
-    version=sys.argv[3]
-    product=utils.get_product_id(specification,version)
-    #Prepare dataset metadata
+    # Read arguments
+    specification = sys.argv[1]
+    container = sys.argv[2]
+    version = sys.argv[3]
+    mozart_rest_url = sys.argv[4]
+    product = utils.get_product_id(specification, version)
+    # Prepare dataset metadata
     metadata = {
         "container": container,
         "job-version": version,
-        "resource":"jobspec"
+        "resource": "jobspec"
     }
-    if not utils.check_exists(container):
-        print("[ERROR] Container, {0}, does not exist. Cannot create HySDS-IO.".format(container),file=sys.stderr)
+    if not utils.check_exists(container, mozart_rest_url):
+        print("[ERROR] Container, {0}, does not exist. Cannot create HySDS-IO.".format(container), file=sys.stderr)
         sys.exit(-2)
-    #Read specification metadata and merge it
-    with open(specification,"r") as fp:
+    # Read specification metadata and merge it
+    with open(specification, "r") as fp:
         metadata.update(json.load(fp))
     metadata["id"] = product
-    hysds_commons.request_utils.requests_json_response("POST", os.path.join(app.conf["MOZART_REST_URL"],"job_spec/add"), data={"spec":json.dumps(metadata)}, verify=False)
+    hysds_commons.request_utils.requests_json_response("POST", os.path.join(
+        mozart_rest_url, "job_spec/add"), data={"spec": json.dumps(metadata)}, verify=False)
     sys.exit(0)
