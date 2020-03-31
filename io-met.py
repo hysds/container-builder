84e1fad36e590a256b17dc07f6479e46207d4b32
Dustin Lo <dustinlo.pcc@gmail.com>
2020-03-30 17:09:37 -0700

    Develop es7 (#12)
    * Hc 123 container builder (#10)
    
    * added gitignore and small formatting changes
    
    * formatting and un-bunching code to look more readable
    
    * using regular requests
    
    * using regular requests
    
    * removed header
    
    * Hc 163 container builder (#11)
    
    * initial commit for cosolidating user_rules and hysds_ios
    
    * code cleanup
    
    * remvoed redundant sys.exit(-1) command in io-met.py

--- io-met.py
+++ io-met.py
@@ -1,40 +1,41 @@
 #!/usr/bin/env python
-
 import os
 import sys
 import json
-import re
 
 from hysds_commons.request_utils import requests_json_response
 import utils
 
 
-def add_hysds_io(rest_url, metadata):
-    """Index hysds-io into respective components ES preserving allowed_accounts
-    if hysds-io already is registered."""
+def add_hysds_io(rest_url, data):
+    """
+    Index hysds-io into respective components ES preserving allowed_accounts
+    if hysds-io already is registered.
+    """
 
     # get hysds-io if exists
-    doc = requests_json_response("GET", os.path.join(rest_url, "hysds_io/type"),
-                                 data={"id": metadata['id']}, verify=False,
-                                 ignore_errors=True)
+    hysds_io_obj = {
+        "id": data['id']
+    }
+    hysds_io_endpoint = os.path.join(rest_url, "hysds_io/type")
+    doc = requests_json_response("GET", hysds_io_endpoint, data=hysds_io_obj, verify=False, ignore_errors=True)
 
     # copy existing allowed accounts
     if doc is not None:
-        merged_accounts = list(set(metadata.get('allowed_accounts', []) +
-                                   doc['result'].get('allowed_accounts', [])))
+        merged_accounts = list(set(data.get('allowed_accounts', []) + doc['result'].get('allowed_accounts', [])))
         if len(merged_accounts) > 0:
-            metadata['allowed_accounts'] = merged_accounts
+            data['allowed_accounts'] = merged_accounts
 
-    # index
-    requests_json_response("POST", os.path.join(rest_url, "hysds_io/add"),
-                           data={"spec": json.dumps(metadata)}, verify=False)
+    data = {
+        "spec": json.dumps(data)
+    }
+    requests_json_response("POST", os.path.join(rest_url, "hysds_io/add"), data=data, verify=False)
 
 
 def usage_and_exit():
     """Prints usage and exit"""
-
-    print("Usage:\n\t{0} <hysds-io> <job-spec> <version> <mozart_rest_url> <grq_rest_url>".format(
-        sys.argv[0]), file=sys.stderr)
+    print("Usage: {0}".format(sys.argv[0]), file=sys.stderr)
+    print("Arguments must be supplied: (hysds-io, job-spec, version, mozart_rest_url, grq_rest_ur)", file=sys.stderr)
     sys.exit(-1)
 
 
@@ -61,16 +62,18 @@ if __name__ == "__main__":
         "resource": "hysds-io-specification"
     }
     if not utils.check_exists(job_spec, mozart_rest_url):
-        print("[ERROR] Job Specification, {0}, does not exist. Cannot create HySDS-IO.".format(
-            job_spec), file=sys.stderr)
+        print("ERROR: Job Specification {0} does not exist. Cannot create HySDS-IO.".format(job_spec), file=sys.stderr)
         sys.exit(-2)
 
     # Read specification metadata and merge it
     with open(specification, "r") as fp:
         metadata.update(json.load(fp))
+
     metadata["id"] = product
+
     if metadata.get("component", "tosca") in ("mozart", "figaro"):
         add_hysds_io(mozart_rest_url, metadata)
     else:
         add_hysds_io(grq_rest_url, metadata)
+
     sys.exit(0)
